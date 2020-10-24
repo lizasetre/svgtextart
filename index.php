b00850c5071292e1b9600098f5c751923d7c8f3f
lizasetre <60122669+lizasetre@users.noreply.github.com>
2020-10-24 14:59:59 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,104 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Text Art</title>
+  <link rel="stylesheet" type="text/css" href="art.css">
+  </head>
+  <body>
+
+
+<svg class="container">
+<svg viewBox="-40 10 700 400">
+	<defs>
+		<path id="R1" fill="none" stroke="red" 
+		d="M 500 150 Q 350 0 350 250 Q 450 300 500 150 " />
+	</defs>
+	<text fill="red">
+		<textPath href="#R1" starOffset="0">
+		I wandered lonely as a cloud that flows on high over vales and hills when all at once
+		<textPath>
+	</text>
+	
+	<defs>
+		<path id="R2" fill="none" stroke="red" 
+		d="M 300 50 Q 50 150 350 250 Q 450 100 300 50  " />
+	</defs>
+	<text fill="orange">
+		<textPath href="#R2" starOffset="0">
+		I saw a crown and hose of golden daffodils beside the lake beneath the trees fluttering and dancing 
+		<textPath>
+	</text>
+	
+	<defs>
+		<path id="R3" fill="none" stroke="red" 
+		d="M 350 250 Q 200 0 150 50 Q 0 200 350 250     " />
+	</defs>
+	<text fill="yellow">
+		<textPath href="#R3" starOffset="0">
+		It started with a budding friendship between two lonely souls,
+      A cautious relationship of two former foes
+		<textPath>
+	</text>
+	
+	<defs>
+		<path id="R4" fill="none" stroke="red" 
+		d="M 500 50 Q 450 0 350 250 Q 750 200 500 50     " />
+	</defs>
+	<text fill="green">
+		<textPath href="#R4" starOffset="0">
+		The blossoming of love's flower covered in delicate thorns. We nurtured and tended it and it soon grew into a
+		<textPath>
+	</text>
+	
+	<defs>
+		<path id="R5" fill="none" stroke="red" 
+		d="M 345 255 Q 375 360 345 495    " />
+	</defs>
+	<text fill="blue">
+		<textPath href="#R5" starOffset="0">
+		blossom of love sweet and true
+	</text>
+	
+	<defs>
+		<path id="R6" fill="red" stroke="red" 
+		d="M 335 245 Q 365 370 335 485  " />
+	</defs>
+	<text fill="indigo">
+		<textPath href="#R6" starOffset="0">
+		My precious flower I am so blessed that I found you
+	</text>
+	
+	<defs>
+		<path id="R7" fill="none" stroke="red" 
+		d="M 360 255 Q 390 360 360 495 " />
+	</defs>
+	<text fill="violet">
+		<textPath href="#R7" starOffset="0">
+		Flower can be compared to our life
+      Leaf will be the hands that always supporting us
+	</text>
+    <defs>
+		<path id="R7" fill="none" stroke="red" 
+		d="M 360 255 Q 390 360 360 495 " />
+	</defs>
+    
+	<text fill="white">
+		<textPath href="#R8" starOffset="0">
+		poem by Mr William Wordsmith
+</text>
+    <defs>
+		<path id="R8" fill="none" stroke="red" 
+		d="M 276 360 Q 300 300 350 400 Q 300 400 276 360 " />
+      </defs>
+    
+    <text fill="white">
+		<textPath href="#R9" starOffset="0">
+		Daffodils
+</text>
+    <defs>
+		<path id="R9" fill="none" stroke="red" 
+		d="M 276 348 Q 312 372 350 410 " />
+      </defs>
+</svg>
+</body>
+</html>
